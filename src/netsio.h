e80538097e2d3115c0d6c6bef3d0874a796857f6
mozzwald <mozzwald@gmail.com>
2026-02-14 08:43:23 -0600

    netsio: Refactor NetSIO RX buffering/control-line handling and harden command frame teardown

--- src/netsio.h
+++ src/netsio.h
@@ -96,6 +96,7 @@ void netsio_poll(void);
 void netsio_wait_for_sync(void);
 int netsio_available(void);
 unsigned int netsio_get_current_baud(void);
+void netsio_apply_control_lines(void);
 int netsio_cold_reset(void);
 int netsio_warm_reset(void);
 
--- src/sio.c
+++ src/sio.c
@@ -1570,6 +1570,7 @@ void SIO_SwitchCommandFrame(int onoff)
 					netsio_send_block(CommandFrame, CommandIndex);
 				*/
 				/* b) Skip sending short/invalid CF */
+				netsio_cmd_off();
 				TransferStatus = SIO_NoFrame;
 			}
 			else
@@ -1751,6 +1752,7 @@ void SIO_PutByte(int byte)
 int NetSIO_GetByte(void)
 {
 	UBYTE b;
+	netsio_apply_control_lines();
 
 	if(netsio_available() > 0)
 	{
